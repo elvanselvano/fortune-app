061d374bea62c7b05c3a6ecf005985c880fac483
viriyaputra <viriyaputra26@gmail.com>
2021-10-11 19:33:45 +0700

    Update button
    Button to predict from the sidebar

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ st.markdown("""
     * Please fill your information on the left side of the application
 """)
 st.header("Prediction Calculation")
+st.markdown("""
+    Press the predict button below to know your result!
+""")
 
 # marital status = notmarried(0),married(1) ; job = businessman(0),college(1),employee(2) ; residence = boarding(0), house(1)
 
@@ -50,53 +53,54 @@ else :
 #print prediction
 result = fortune_model.predict([[sidebar_age, sidebar_income, marital_number, job_number, residence_number]])
 
-if result == 1:
-    consumption_calculation = sidebar_income * 0.75
-    savings_calculation = sidebar_income * 0.15
-    investment_calculation = sidebar_income * 0.1
-    st.markdown(f"""
-    From our prediction, we conclude you must split your income into this : 
-    * 75% for Consumption = Rp{round(consumption_calculation):,}
-    * 15% for Savings = Rp{round(savings_calculation):,}
-    * 10% for Investment = Rp{round(investment_calculation):,}
-    """)
-elif result == 2:
-    consumption_calculation = sidebar_income * 0.65
-    savings_calculation = sidebar_income * 0.2
-    investment_calculation = sidebar_income * 0.15
-    st.markdown(f"""
-    From our prediction, we conclude you must split your income into this : 
-    * 65% for Consumption = Rp{round(consumption_calculation):,}
-    * 20% for Savings = Rp{round(savings_calculation):,}
-    * 15% for Investment = Rp{round(investment_calculation):,}
-    """)
-elif result == 3:
-    consumption_calculation = sidebar_income * 0.6
-    savings_calculation = sidebar_income * 0.25
-    investment_calculation = sidebar_income * 0.15
-    st.markdown(f"""
-    From our prediction, we conclude you must split your income into this : 
-    * 60% for Consumption = Rp{round(consumption_calculation):,}
-    * 25% for Savings = Rp{round(savings_calculation):,}
-    * 15% for Investment = Rp{round(investment_calculation):,}
-    """)
-elif result == 4:
-    consumption_calculation = sidebar_income * 0.55
-    savings_calculation = sidebar_income * 0.3
-    investment_calculation = sidebar_income * 0.15
-    st.markdown(f"""
-    From our prediction, we conclude you must split your income into this : 
-    * 55% for Consumption = Rp{round(consumption_calculation):,}
-    * 30% for Savings = Rp{round(savings_calculation):,}
-    * 15% for Investment = Rp{round(investment_calculation):,}
-    """)
-else :
-    consumption_calculation = sidebar_income * 0.4
-    savings_calculation = sidebar_income * 0.3
-    investment_calculation = sidebar_income * 0.3
-    st.markdown(f"""
-    From our prediction, we conclude you must split your income into this : 
-    * 40% for Consumption = Rp{round(consumption_calculation):,}
-    * 30% for Savings = Rp{round(savings_calculation):,}
-    * 30% for Investment = Rp{round(investment_calculation):,}
-    """)
+if st.button('Predict'):
+    if result == 1:
+        consumption_calculation = sidebar_income * 0.75
+        savings_calculation = sidebar_income * 0.15
+        investment_calculation = sidebar_income * 0.1
+        st.markdown(f"""
+        From our prediction, we conclude you must split your income into this : 
+        * 75% for Consumption = Rp{round(consumption_calculation):,}
+        * 15% for Savings = Rp{round(savings_calculation):,}
+        * 10% for Investment = Rp{round(investment_calculation):,}
+        """)
+    elif result == 2:
+        consumption_calculation = sidebar_income * 0.65
+        savings_calculation = sidebar_income * 0.2
+        investment_calculation = sidebar_income * 0.15
+        st.markdown(f"""
+        From our prediction, we conclude you must split your income into this : 
+        * 65% for Consumption = Rp{round(consumption_calculation):,}
+        * 20% for Savings = Rp{round(savings_calculation):,}
+        * 15% for Investment = Rp{round(investment_calculation):,}
+        """)
+    elif result == 3:
+        consumption_calculation = sidebar_income * 0.6
+        savings_calculation = sidebar_income * 0.25
+        investment_calculation = sidebar_income * 0.15
+        st.markdown(f"""
+        From our prediction, we conclude you must split your income into this : 
+        * 60% for Consumption = Rp{round(consumption_calculation):,}
+        * 25% for Savings = Rp{round(savings_calculation):,}
+        * 15% for Investment = Rp{round(investment_calculation):,}
+        """)
+    elif result == 4:
+        consumption_calculation = sidebar_income * 0.55
+        savings_calculation = sidebar_income * 0.3
+        investment_calculation = sidebar_income * 0.15
+        st.markdown(f"""
+        From our prediction, we conclude you must split your income into this : 
+        * 55% for Consumption = Rp{round(consumption_calculation):,}
+        * 30% for Savings = Rp{round(savings_calculation):,}
+        * 15% for Investment = Rp{round(investment_calculation):,}
+        """)
+    else :
+        consumption_calculation = sidebar_income * 0.4
+        savings_calculation = sidebar_income * 0.3
+        investment_calculation = sidebar_income * 0.3
+        st.markdown(f"""
+        From our prediction, we conclude you must split your income into this : 
+        * 40% for Consumption = Rp{round(consumption_calculation):,}
+        * 30% for Savings = Rp{round(savings_calculation):,}
+        * 30% for Investment = Rp{round(investment_calculation):,}
+        """)
