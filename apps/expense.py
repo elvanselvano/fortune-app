efacb257b05763fdfdb90800e44090b919c44e14
viriyaputra <viriyaputra26@gmail.com>
2021-11-07 05:21:07 +0700

    Adding Piechart (need continue)
    Pie chart added but still need to be improve and fixed.
    Things to improve is
    1. removing percent(autopct) of uncalled label
    2. grouping Income,Expense,Saving as a 3 different group and use explode (to make it easier to look and differentiate)
    3. make the legend split by group

--- apps/expense.py
+++ apps/expense.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
 def app():
     
@@ -28,6 +29,8 @@ def app():
         Easy way to count your monthly expense with Fortune, fill your income and expense on the left bar to see the result.
     """)
 
+
+
     #print calculator and pie chart (kalo bisa bikin reset button)
     total_income = sidebar_monthIncome + sidebar_monthOtherIncome
     total_expense = sidebar_houseExpense + sidebar_transportExpense + sidebar_foodExpense + sidebar_healthExpense + sidebar_childExpense + sidebar_wifeExpense + sidebar_selfExpense + sidebar_installmentExpense
@@ -35,7 +38,20 @@ def app():
     calculator_result = total_income - total_expense - total_saving
 
     if st.button("Calculate"):
+        #creating piechart
+        labels = ["Monthly Pay", "Other Monthly Income", "Housing", "Transportation", "Food", "Health", "Children", "Wife", "Self", "Installment", "Emergency", "Retirement", "Others"]
+        values = [sidebar_monthIncome, sidebar_monthOtherIncome, sidebar_houseExpense, sidebar_transportExpense, sidebar_foodExpense, sidebar_healthExpense, sidebar_childExpense, sidebar_wifeExpense, sidebar_selfExpense, sidebar_installmentExpense, sidebar_emergencySaving, sidebar_retirementSaving, sidebar_otherSaving]
+        pie_colors = ["#008000", "#F0E68C", "#F08080", "#D3D3D3", "#FFA500", "#800080", "#A0522D", "#FF6347", "#DC143C", "#6495ED", "#7FFFD4", "#E9967A", "#483D8B"]
+        fig1, ax1 = plt.subplots(figsize =(10, 5))
+        ax1.pie(values, autopct='%1.1f%%', colors=pie_colors)
+        ax1.axis('equal')
+        ax1.legend(labels, title="Legend", loc="center left",bbox_to_anchor=(1, 0, 0.5, 1))
+
         if(calculator_result > 0):
             st.write(f"Wow, you still have Rp{round(calculator_result):,} left. It is a great step, keep it going!!")
+            st.pyplot(fig1)
         elif(calculator_result < 0):
-            st.write(f"Your money is over budgeting for Rp{abs(round(calculator_result)):,}. Be careful and try to minimize your expense as you can.")
+            st.write(f"Your money is over budget for Rp{abs(round(calculator_result)):,}. Be careful and try to minimize your expense as you can.")
+            st.pyplot(fig1)
+        else :
+            st.write(f"You have equal income and expense, maybe try to reduce some of your non-priority expense")
